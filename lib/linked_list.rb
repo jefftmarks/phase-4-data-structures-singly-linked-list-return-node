ee5043672ab8d99fb4c267f4efc7a121db8d9de0
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-25 21:27:17 -0400

    done with assignment

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head
@@ -8,7 +9,41 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+		return nil if n > self.size || n < 1
+
+		target = self.size - n
+		last_node = self.head
+		count = 0
+
+		while count < target
+			last_node = last_node.next_node
+			count += 1
+		end
+
+		last_node.value
+	
   end
 
+	def size
+		return 0 unless self.head
+
+		last_node = self.head
+		count = 1
+
+		while last_node.next_node
+			last_node = last_node.next_node
+			count += 1
+		end
+
+		count
+		
+	end
+
 end
+
+linked_list = LinkedList.new
+linked_list.head = Node.new(1)
+linked_list.head.next_node = Node.new(2)
+linked_list.head.next_node.next_node = Node.new(3)
+
+# binding.pry
